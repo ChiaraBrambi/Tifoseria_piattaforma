a5a338fa1c50b7105a44cdf546619aea847f5bd2
chiara <chiara7.brambilla@mail.polimi.it>
2020-11-15 18:40:18 +0100

    testo

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let stadioIcon;
 let trombaIcon;
 let sfondo;
-let tt = 'Happy Halloween';
-let tt2 = 'Quanto sei zuccone?  Urla Boooooo per far salire lo ZuccoMetro'
-let tt3 = '- ZuccoMetro'
-let tt4 = 'Press enter for result'
+let palette =['#F9F9F9','#D5D0D3','#B7AEB5', '#877B85'];
+let p,p1;
+let tt3 = 'cordinazione';
+let tt4 = 'capienza';
+
+
 function preload(){
 sfondo = loadImage("./assets/SFONDO.png");
 stadioIcon = loadImage("./assets/stadio.png");
@@ -13,11 +15,32 @@ trombaIcon = loadImage("./assets/trombetta.png");
 
 function setup() {
 createCanvas(windowWidth,windowHeight)
-//background('Cultured');
-background(sfondo);
+background('#F9F9F9');
+//background(sfondo);
+imageMode(CENTER);
+
+//text
+fill('#877B85');//4PALETTE
+textAlign(CENTER, TOP);//text(str,x,y,[x2],[y2])
+textStyle(BOLD);
+text('COOD O1',width/2,height/11);
+text('Squadra1-Squdra2',width/2,height/9);
+text('capienza',width/7*6,height/9);
+
+
+
+//pittogrammi
+   image(stadioIcon, width/7*6,height/7, stadioIcon.width/7 , stadioIcon.height/7);
+   image(trombaIcon, width/2,height/2, trombaIcon.width/7 , trombaIcon.height/7);
+  //barre
+  //ellipse(width/2,height/2,10,width/4);
+  rectMode(CENTER);
+  noStroke();
+  fill('#D5D0D3');
+  rect(width/7,height/2,10,width/3.5);
+  rect(width/7*6,height/2,10,width/3.5);
 }
 
 function draw() {
 
-   image(stadioIcon, width/2,height/2, stadioIcon.width , stadioIcon.height);
 }
