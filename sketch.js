2056192d46750ba16fed7244441304fd3f161a84
chiara <chiara7.brambilla@mail.polimi.it>
2020-11-15 16:50:20 +0100

    prova

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
 let stadioIcon;
 let trombaIcon;
+let sfondo;
 let tt = 'Happy Halloween';
 let tt2 = 'Quanto sei zuccone?  Urla Boooooo per far salire lo ZuccoMetro'
 let tt3 = '- ZuccoMetro'
 let tt4 = 'Press enter for result'
 function preload(){
+sfondo = loadImage("./assets/SFONDO.png");
 stadioIcon = loadImage("./assets/stadio.png");
 trombaIcon = loadImage("./assets/trombetta.png");
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-background('Cultured');
+createCanvas(windowWidth,windowHeight)
+//background('Cultured');
+background(sfondo);
 }
 
-function draw() {}
+function draw() {
+
+   image(stadioIcon, width/2,height/2, stadioIcon.width , stadioIcon.height);
+}
