6dd7d25ca169f77539e85549bdbf1fa549b56e24
chiara <chiara7.brambilla@mail.polimi.it>
2020-11-16 12:27:20 +0100

    manca barra pazza

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let stadioIcon, trombaIcon, tscuraIcon;//icone
-let h;//altezza barra percentuale
+let h = 20;//altezza barra %;//altezza barra percentuale
 let pos;//posizione
 let palette = ['#F9F9F9', '#D5D0D3', '#B7AEB5', '#877B85'];
 
@@ -8,8 +8,8 @@ let alt = 1; //h dei rettangoli suono
 let i = 0; //regola ogni quanto cambia alt
 let p_coord = 0; //var coordinazione
 
-let n_trombetta = 0.1; //var piattaforma: quando alt!=1 viene incrementata
-let n_interazione = 0.1; //var utente usa la trobetta, preme bottone
+let n_trombetta = 0; //var piattaforma: quando alt!=1 viene incrementata
+let n_interazione = 0; //var utente usa la trobetta, preme bottone
 //se faccio ntrombetta/niterazione trovo la coordinazione
 
 /////////////////////////////////////////////////////////////////////////
@@ -50,25 +50,41 @@ function draw() {
   text('cordinazione', pos, height / 5);
 
   //percentuale
-  text('50%', pos*5, height / 5*4.5);
-  text('83%', pos, height / 5*4.5);
+
+
+  text('63%', pos*5, height / 5*4.5);//dx
 
   image(stadioIcon, pos*5, height / 7, stadioIcon.width / 7, stadioIcon.height / 7);//stadio
 
-  h=200;//altezza barra %
-  fill('#D5D0D3');
+  text( p_coord + '%' , pos, height / 5*4.5);//sx
+  console.log(' interazione ' + n_interazione);
+  console.log(' trombetta ' + n_trombetta);
 
+  //PER LA BARRA DELLA PERCENTUALE
+      //interazione utente, temporaneamente tasto ENTER
+      if (alt != 1 & keyIsDown(ENTER)) {
+        n_interazione += 4;// per far tornare la percentuale in pai alla trombetta
+        p_coord = round((n_interazione / n_trombetta)*100);
+      } else if (alt = 1) {
+        n_interazione = 0;
+        p_coord = 0;
+      }
+
+
+//BARRE GRIGIA E VIOLA
+  fill('#D5D0D3');
   //barre grige
   rectMode(CENTER);
-  rect(width /6, height / 6*3.3 , 20, width / 3.5, 20); //rect(x,y,w,h,[tl])
+  rect(width /6, height / 6*3.3 , 20, width / 3.5, 20); //rect(x,y,w,h,[tl]) h tot= 439 =  width / 3.5
   rect(width /6*5 , height /  6*3.3 , 20, width / 3.5, 20);
 
+  h = ((width / 3.5) /100)*p_coord; //altezza barra %
 push();
       //barre viola
       rectMode(CORNER);
       fill('#877B85');
       rect(width /6-10, height / 6*3.3 + width /7 , 20, -h, 20);//BARRA SX
-      rect(width /6*5-10,  height / 6*3.3 + width /7 , 20, -h, 20);//BARRA DX
+      rect(width /6*5-10,  height / 6*3.3 + width /7 , 20, -300, 20);//BARRA DX
 pop();
 
 //barre laterali suono trombetta
@@ -81,7 +97,6 @@ pop();
       if (i % 2 != 0) { //quando i è dispari altezza deve diventare 1*random
         alt = 1 * random(2, 10);
         n_trombetta++;
-        console.log( n_trombetta);
       } else {
         alt = 1;
         n_trombetta = 0.1;
@@ -110,6 +125,7 @@ noFill( );
     ellipse(width / 2, height / 2, 90);//cerchio centrale
     pop();
     }
+
 }
 
 
